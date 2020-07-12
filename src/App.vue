6bf5ac90b77aeb36571e9dc903851b5421470ee5
bev1 <longer30001@gmail.com>
2020-07-12 20:52:00 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,154 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-overlay v-if="isFetching">
+    <v-progress-circular
+      :size="50"
+      color="primary"
+      indeterminate
+    ></v-progress-circular>
+    </v-overlay>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/logo.png"
+          width="40"
+        />
+      </div>
+      <v-form>
+        <v-text-field
+          v-model="searchText"
+          label="Search"
+          hide-details="true"
+          @input="search"
+        ></v-text-field>
+      </v-form>
+    </v-app-bar>
+
+    <v-main>
+      <v-img
+        v-if="notFound"
+        :src="notFoundURL"
+      />
+      <MainContent v-bind:gifs="gifs"/>
+      <v-btn v-if="loadMore" depressed color="primary" @click="loadMoreGiphs">Load More</v-btn>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainContent from './components/MainContent';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  components: {
+    MainContent,
+  },
+
+  data: () => ({
+    isFetching: false,
+    gifs: [],
+    searchText: '',
+    notFound: false,
+    notFoundURL: '',
+    loadMore: false,
+    counterForPagination: 0,
+  }),
+
+  created: function() {
+    const getGiphs = () => {
+      this.isFetching = true;
+      fetch('https://api.giphy.com/v1/gifs/trending?api_key=PhRv8VCfrXRbCSS12cHSopUs4hu7OuSr')
+      .then(response => response.json())
+      .then(data => {
+        data.data.forEach(element => {
+          this.gifs.push(element)
+        });
+      })
+      .then(() => {
+        this.isFetching = false;
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+    getGiphs()
+    const getRandomNotFound = () => {
+      fetch('https://api.giphy.com/v1/gifs/random?api_key=PhRv8VCfrXRbCSS12cHSopUs4hu7OuSr&tag=notFound')
+      .then(response => response.json())
+      .then(data => {
+        this.notFoundURL = data.data.images.original.url;
+      })
+      .catch(err => {
+        console.log(err)
+      })      
+    }
+    getRandomNotFound()
+  },
+
+  methods: {
+    search: function() {
+      this.isFetching = true;
+      fetch(`https://api.giphy.com/v1/gifs/search?q=${this.searchText}&api_key=PhRv8VCfrXRbCSS12cHSopUs4hu7OuSr&offset=0`)
+      .then(response => response.json())
+      .then(data => {
+        if(data.data.length) {
+          this.notFound = false;
+        } else {
+          this.notFound = true
+        }
+        let newGiphs = []
+        data.data.forEach(element => {
+          newGiphs.push(element)
+        });
+        this.gifs = newGiphs;
+        if (data.pagination.total_count > this.gifs.length) {
+          this.loadMore = true;
+        } else {
+          this.loadMore = false;
+        }
+      })
+      .then(() => {
+        this.isFetching = false;
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    },
+    loadMoreGiphs: function() {
+      this.isFetching = true;
+      fetch(`https://api.giphy.com/v1/gifs/search?q=${this.searchText}&api_key=PhRv8VCfrXRbCSS12cHSopUs4hu7OuSr&offset=${this.counterForPagination+25}`)
+      .then(response => response.json())
+      .then(data => {
+        data.data.forEach(element => {
+          this.gifs.push(element)
+        });
+        if (data.pagination.total_count > this.gifs.length) {
+          this.loadMore = true;
+        } else {
+          this.loadMore = false;
+        }
+        this.counterForPagination = this.counterForPagination + 25;
+      })
+      .then(() => {
+        this.isFetching = false;
+      })
+      .catch(err => {
+        console.log(err)
+        })
+    }
+  }
+};
+</script>
+<style scoped>
+form {
+  width: 100%;
 }
 </style>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,151 @@
+<template>
+  <v-container>
+    <!-- <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a
+            href="https://community.vuetifyjs.com"
+            target="_blank"
+          >Discord Community</a>
+        </p>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          What's next?
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Important Links
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(link, i) in importantLinks"
+            :key="i"
+            :href="link.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ link.text }}
+          </a>
+        </v-row>
+      </v-col>
+
+      <v-col
+        class="mb-5"
+        cols="12"
+      >
+        <h2 class="headline font-weight-bold mb-3">
+          Ecosystem
+        </h2>
+
+        <v-row justify="center">
+          <a
+            v-for="(eco, i) in ecosystem"
+            :key="i"
+            :href="eco.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ eco.text }}
+          </a>
+        </v-row>
+      </v-col>
+    </v-row> -->
+    
+    <v-container class="grey lighten-5">
+        <transition-group
+          tag="div"
+          class="row"
+          name="list"
+        >
+          <v-col
+            v-for="gif in gifs"
+            :key="gif.id"
+            xs="12"
+            sm="6"
+            md="4"
+          >
+            <v-sheet min-height="250">
+              <v-lazy>
+                <v-img
+                  :src="gif.images.original.url"
+                  lazy-src='../assets/preloader.gif'
+                />
+              </v-lazy>
+              <v-btn depressed color="primary" @click="share(gif)">Share</v-btn>
+            </v-sheet>
+          </v-col>
+        </transition-group>
+    </v-container>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'MainContent',
+
+    props: [
+      'gifs'
+    ],
+
+    data: () => ({
+
+    }),
+
+    methods: {
+      share: function(gif) {
+        navigator.share({
+          text: 'GIF',
+          url: gif.images.original.url
+        })
+      }
+    },
+  }
+</script>
+
+<style scoped>
+.list-enter-active, .list-leave-active {
+  transition: all 1s;
+}
+.list-enter, .list-leave-to {
+  opacity: 0;
+  transform: translateY(80px);
+}
+</style>
